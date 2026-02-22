fb5d333898c885809aaa43b56a2695bb087d7ad6
Rattasat Phabsing <jkung1998@gmail.com>
2026-02-22 14:02:04 +0700

    name

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -204,7 +204,7 @@ export const ProfilePage = ({ isDark }) => {
                             className="hidden"
                         />
 
-                        <h1 className="text-2xl font-bold font-serif mt-4">{user?.email}</h1>
+                        <h1 className="text-2xl font-bold font-serif mt-4">{user?.name}</h1>
                         <p className={`text-sm mt-1 ${isDark ? 'text-slate-400' : 'text-slate-500'}`}>จัดการข้อมูลส่วนตัว</p>
                     </div>
 
